3c75ba08da857c41dda55da4deed8899a14fa8f8
soonable@naver.com <soonable@naver.com>
2023-06-26 21:43:04 +0900

    feat : 인터페이스에 정적 메서드

--- src/main/java/com/soon/effectivejavastudy/chapter1/item01/HelloService.java
+++ src/main/java/com/soon/effectivejavastudy/chapter1/item01/HelloService.java
@@ -4,6 +4,20 @@ public interface HelloService {
 
     String hello();
 
+    static String bye() {
+        prepareMessage();
+        return "bye";
+    }
+
+    private static void prepareMessage() {
+        // 내부적인 용도
+    }
+
+    default String hi() {
+        prepareMessage();
+        return "hi";
+    }
+
 //    interface 에선 public 간주
     static HelloService of(String lang) {
         if (lang.equals("ko")) {
---
+++ src/main/java/com/soon/effectivejavastudy/chapter1/item01/ListQuiz.java
@@ -0,0 +1,28 @@
+package com.soon.effectivejavastudy.chapter1.item01;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
+public class ListQuiz {
+
+    public static void main(String[] args) {
+        //Q1
+        ArrayList<Integer> numbers = new ArrayList<>();
+        numbers.add(5);
+        numbers.add(4);
+        numbers.add(1);
+        numbers.add(100);
+
+        // 1, 4, 5, 100
+        Comparator<Integer> desc = (o1, o2) -> o2 - o1;
+
+        numbers.sort(desc);
+        System.out.println(numbers);
+
+        //Q2
+        Comparator<Integer> asc = desc.reversed();
+        numbers.sort(asc);
+        System.out.println(numbers);
+    }
+}
