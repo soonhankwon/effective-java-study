981e645b7367b941c9756e937376285b1d29438d
soonhankwon <soonable@gmail.com>
2023-07-24 08:53:48 +0900

    item15 : 클래스와 인터페이스의 접근 제한자 사용원칙

---
+++ src/main/java/com/soon/effectivejavastudy/chapter2/item15/class_and_interfaces/member/Member.java
@@ -0,0 +1,4 @@
+package com.soon.effectivejavastudy.chapter2.item15.class_and_interfaces.member;
+
+public class Member {
+}
---
+++ src/main/java/com/soon/effectivejavastudy/chapter2/item15/class_and_interfaces/member/MemberService.java
@@ -0,0 +1,5 @@
+package com.soon.effectivejavastudy.chapter2.item15.class_and_interfaces.member;
+
+public interface MemberService {
+
+}
