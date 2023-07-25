425f276656070655adc5964f1d5cca9d01393ab5
soonhankwon <soonable@gmail.com>
2023-07-25 11:50:05 +0900

    item17 : 불변 클래스 장점, 단점

---
+++ src/main/java/com/soon/effectivejavastudy/chapter2/item17/BigIntExample.java
@@ -0,0 +1,17 @@
+package com.soon.effectivejavastudy.chapter2.item17;
+
+import com.soon.effectivejavastudy.chapter2.item16.method.Point;
+
+import java.math.BigInteger;
+import java.util.HashSet;
+import java.util.Set;
+
+public class BigIntExample {
+
+    public static void main(String[] args) {
+        BigInteger ten = BigInteger.TEN;
+        BigInteger minusTen = ten.negate();
+
+        final Set<Point> numbers = new HashSet<>();
+    }
+}
---
+++ src/main/java/com/soon/effectivejavastudy/chapter2/item17/StringExample.java
@@ -0,0 +1,10 @@
+package com.soon.effectivejavastudy.chapter2.item17;
+
+public class StringExample {
+
+    public static void main(String[] args) {
+        String name = "soon";
+        StringBuilder sb = new StringBuilder(name);
+        sb.append("han");
+    }
+}
