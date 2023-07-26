5153758a06ffab59a2b68b973907af8d1f23cb2f
soonhankwon <soonable@gmail.com>
2023-07-26 12:31:43 +0900

    item17 : CountDownLatch

---
+++ src/main/java/com/soon/effectivejavastudy/chapter2/item17/concurrent/ConcurrentExample.java
@@ -0,0 +1,54 @@
+package com.soon.effectivejavastudy.chapter2.item17.concurrent;
+
+import java.util.concurrent.CountDownLatch;
+
+public class ConcurrentExample {
+
+    public static void main(String[] args) throws InterruptedException {
+        int N = 10;
+        CountDownLatch startSignal = new CountDownLatch(1);
+        CountDownLatch doneSignal = new CountDownLatch(N);
+
+        for (int i = 0; i < N; ++i)
+            new Thread(new Worker(startSignal, doneSignal)).start();
+
+        ready();
+        startSignal.countDown();
+        doneSignal.await();
+        done();
+
+    }
+
+    private static void ready() {
+        System.out.println("READY");
+    }
+
+    private static void done() {
+        System.out.println("END");
+    }
+
+    private static class Worker implements Runnable {
+
+        private final CountDownLatch startSignal;
+        private final CountDownLatch doneSignal;
+
+        public Worker(CountDownLatch startSignal, CountDownLatch doneSignal) {
+            this.startSignal = startSignal;
+            this.doneSignal = doneSignal;
+        }
+
+        @Override
+        public void run() {
+            try {
+                startSignal.await();
+                doWork();
+                doneSignal.countDown();
+            } catch (InterruptedException e) {
+            }
+        }
+
+        void doWork() {
+            System.out.println("working thread: " + Thread.currentThread().getName());
+        }
+    }
+}
