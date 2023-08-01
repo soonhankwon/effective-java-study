07336cd3b3fba18019eda1f774b4f95eef2396cd
soonhankwon <soonable@gmail.com>
2023-08-01 09:44:50 +0900

    item25 : 톱 레벨 클래스는 한 파일에 하나만 담으라

---
+++ src/main/java/com/soon/effectivejavastudy/chapter2/item25/Utensil.java
@@ -0,0 +1,10 @@
+package com.soon.effectivejavastudy.chapter2.item25;
+
+// 두 클래스가 한 파일에 정의되었다. - DONT DO THAT
+class Utensil {
+    static final String NAME = "pan";
+}
+
+class Dessert {
+    static final String NAME = "cake";
+}
