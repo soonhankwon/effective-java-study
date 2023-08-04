e71670873a7a5e8bb585466659d4e5ec0392560d
soonhankwon <soonable@gmail.com>
2023-08-04 21:31:04 +0900

    item30 : 이왕이면 제네릭 메서드로 만들라

--- src/main/java/com/soon/effectivejavastudy/chapter2/item30/after/GenericSingletonFactory.java
+++ src/main/java/com/soon/effectivejavastudy/chapter2/item30/after/GenericSingletonFactory.java
@@ -1,6 +1,5 @@
 package com.soon.effectivejavastudy.chapter2.item30.after;
 
-import java.util.function.Function;
 import java.util.function.UnaryOperator;
 
 // 제네릭 싱글톤 팩터리 패턴
@@ -16,13 +15,13 @@ public class GenericSingletonFactory {
     // 제네릭 싱글턴을 사용하는 예
     public static void main(String[] args) {
         String[] strings = {"a", "b", "c"};
-        Function<String, String> sameString = identityFunction();
+        UnaryOperator<String> sameString = identityFunction();
         for (String s : strings) {
             System.out.println(sameString.apply(s));
         }
 
         Number[] numbers = {1, 2.0, 3L};
-        Function<Number, Number> sameNumber = identityFunction();
+        UnaryOperator<Number> sameNumber = identityFunction();
         for (Number n : numbers) {
             System.out.println(sameNumber.apply(n));
         }
