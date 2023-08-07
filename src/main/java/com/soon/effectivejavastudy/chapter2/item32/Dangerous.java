b64e5dba15bd714e283607f17213bc48cff6def7
soonhankwon <soonable@gmail.com>
2023-08-07 10:43:48 +0900

    item32 : 제네릭과 가변인수를 함께 쓸 때는 신중하라.

---
+++ src/main/java/com/soon/effectivejavastudy/chapter2/item32/Dangerous.java
@@ -0,0 +1,19 @@
+package com.soon.effectivejavastudy.chapter2.item32;
+
+import java.util.List;
+
+// 제네릭 varargs 배열 매개변수에 값을 저장하는 것은 안전하지 않다.
+public class Dangerous {
+
+    // 제네릭과 varargs 를 혼용하면 타입 안전성이 깨진다.
+    static void dangerous(List<String>... stringLists) {
+        List<Integer> intList = List.of(42);
+        Object[] objects = stringLists;
+        objects[0] = intList; //힙 오염 발생
+        String s = stringLists[0].get(0); // ClassCastException
+    }
+
+    public static void main(String[] args) {
+        dangerous(List.of("There be dragons!"));
+    }
+}
---
+++ src/main/java/com/soon/effectivejavastudy/chapter2/item32/FlattenWithVarargs.java
@@ -0,0 +1,23 @@
+package com.soon.effectivejavastudy.chapter2.item32;
+
+import java.util.ArrayList;
+import java.util.List;
+
+// 제네릭 varargs 매개변수를 안전하게 사용하는 메서드
+public class FlattenWithVarargs {
+
+    @SafeVarargs
+    static <T> List<T> flatten(List<? extends T>... lists) {
+        List<T> result = new ArrayList<>();
+        for (List<? extends T> list : lists) {
+            result.addAll(list);
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        List<Integer> flatList = flatten(
+                List.of(1,2), List.of(3,4,5), List.of(6,7));
+        System.out.println(flatList);
+    }
+}
